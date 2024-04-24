767ac283d108c1e02295456536753dda0edb183e
Son Do <dominhson580@gmail.com>
2024-04-24 23:22:30 +0700

    Fix typo

--- examples/2-AbstractionsWithData/2.4-MultipleRepresentations/message-passing.scm
+++ examples/2-AbstractionsWithData/2.4-MultipleRepresentations/message-passing.scm
@@ -16,7 +16,7 @@
   (define (dispatch op)
     (cond ((eq? op 'real-part) x)
           ((eq? op 'imag-part) y)
-          ((eq? op 'magniture) (sqrt (+ square x) (+ square y)))
+          ((eq? op 'magnitude) (sqrt (+ square x) (+ square y)))
           ((eq? op 'angle) (atan y x))
           (else (error "Unknown op: MAKE-FROM-REAL-IMAG" op))))
   dispatch)
