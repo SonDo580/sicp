9e1082b6c04d446aeaf782bdb3a4d895e1391009
Son Do <dominhson580@gmail.com>
2024-06-28 23:41:35 +0700

    Note - multiple shared resources

---
+++ notes/3-Modularity,Objects,State/3.4-Concurrency/multiple-shared-resources.scm
@@ -0,0 +1,67 @@
+;; From note 3.4/serializer
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposit) (protected deposit))
+            ((eq? m 'balance) balance))
+            (else (error "Unknown request: MAKE-ACCOUNT" m))))
+    dispatch)
+
+;; Suppose we wish to swap the balances in 2 bank accounts
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
+
+;; - The procedure works well when only a single process is doing the exchange
+;; - We must arrange for the exchange procedure to lock out any other 
+;; concurrent accesses to the accounts during the time of the exchange.
+
+;; => Solution: 
+;; using both accounts' serializers to serialize the entire exchange procedure.
+
+(define (make-account-and-serializer balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((serializer (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) serializer)
+            (else (error "Unknown request: MAKE-ACCOUNT" m))))
+    dispatch))
+
+(define (deposit account amount)
+  (let ((s (account 'serializer))
+        (d (account 'deposit)))
+    ((s d) amount)))
+
+(define (withdraw account amount)
+  (let ((s (account 'serializer))
+        (w (account 'withdraw)))
+    ((s w) amount)))
+
+;; Serialize the 'exchange' procedure
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    ((serializer1 (serializer2 exchange))
+     account1
+     account2)))
