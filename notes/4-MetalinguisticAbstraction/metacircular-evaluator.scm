5757dcc31cd79922388376b50102f1c077263bc0
Son Do <dominhson580@gmail.com>
2024-08-03 16:49:41 +0700

    metacircular evaluator: represent expressions

--- notes/4-MetalinguisticAbstraction/metacircular-evaluator.scm
+++ notes/4-MetalinguisticAbstraction/metacircular-evaluator.scm
@@ -2,6 +2,7 @@
 ;; an evaluator that is written in the same language that it evaluates
 
 ;; 1) CORE
+
 ;; Eval
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
@@ -65,3 +66,118 @@
     (eval (definition-value exp) env)
     env)
   'ok)
+
+;; 2) REPRESENTING EXPRESSIONS
+
+;; Self-evaluating: numbers and strings
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        (else false)))
+
+;; Variable: represented by symbols
+(define (variable? exp) (symbol? exp))
+
+;; Quotation: '<text> <-> (quote <text>)
+(define (quoted? exp) (tagged-list? exp 'quote))
+(define (text-of-quotation exp) (cadr exp))
+
+(define (tagged-list exp tag)
+  (if (pair? exp)
+    (eq? (car exp) tag)
+    false))
+
+;; Assignment: (set! <var> <value>)
+(define (assignment? exp) (tagged-list? exp 'set!))
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
+
+;; Definition: 2 forms
+;; 1. (define <var> <value>)
+;; 2. (define (<var> <...params>) 
+;;         <body>)
+;; 
+;; form 2 is syntactic sugar for:
+;; (define <var>
+;;         (lambda (<...params>)
+;;                 <body>))
+(define (definition? exp) (tagged-list? exp 'define))
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+    (cadr exp) ; form 1
+    (caadr exp))) ; form 2
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+    (caddr exp) ; form 1
+    (make-lambda (cdadr exp) ; parameters
+                 (cddr exp)))) ; body
+
+;; Lambda expression
+(define (lambda? exp) (tagged-list? exp 'lambda))
+(define (lambda-parameters exp) (cadr exp))
+(define (lambda-body exp) (cddr exp))
+
+;; lambda expression constructor (used by definition-value)
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+;; If: (if <predicate> <consequent> <alternative>?)
+(define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+    (cadddr exp)
+    'false))
+
+;; if expression constructor (used by cond->if)
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+;; Begin: (begin <...expressions>)
+(define (begin? exp) (tagged-list? exp 'begin))
+(define (begin-actions exp) (cdr exp))
+(define (last-exp? seq) (null? (cdr seq)))
+(define (first-exp seq) (car seq))
+(define (rest-exps seq) (cdr seq))
+
+;; constructor sequence->exp (used by cond->if)
+;; transform a sequence into a single expression
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exp? seq) (first-exp seq))
+        (else (make-begin seq))))
+(define (make-begin seq) (cons 'begin seq))
+
+;; Procedure applications: (<operator> <...operands>)
+(define (application? exp) (pair? exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+(define (no-operands? ops) (null? ops))
+(define (first-operand ops) (car ops))
+(define (rest-operands ops) (cdr ops))
+
+;; DERIVED EXPRESSIONS
+
+;; Cond: (cond <...clauses>)
+;; clause format: (<predicate>/else <...expresions>)
+;; => transformed to nested if
+(define (cond? exp) (tagged-list? exp 'cond))
+(define (cond-clauses exp) (cdr exp))
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+(define (cond-predicate clause) (car clause))
+(define (cond-actions clause) (cdr clause))
+(define (cond->if exp) (expand-clauses (cond-clauses exp)))
+(define (expand-clauses clauses)
+  (if (null? clauses)
+    'false ; no else clause
+    (let ((first (car clauses))
+          (rest (cdr clauses)))
+      (if (cond-else-clause? first)
+        (if (null? rest)
+          (sequence->exp (cond-actions first))
+          (error "ELSE clause isn't last: COND->IF" clauses))
+        (make-if (cond-predicate first)
+                 (sequence->exp (cond-action first))
+                 (expand-clauses rest))))))
