4dbfd9f0901b37d9101eaf31f3584a41f4f75ab7
Son Do <dominhson580@gmail.com>
2024-09-26 23:38:58 +0700

    Register machines - action

--- notes/5-RegisterMachines/5.1-DesignRegisterMachines/design-language.scm
+++ notes/5-RegisterMachines/5.1-DesignRegisterMachines/design-language.scm
@@ -37,4 +37,24 @@
   (assign a (reg b))
   (assign b (reg t))
   (goto (label test-b))
- gcd-done)
+ gcd-done)  
+
+;; GCD machine that reads inputs and prints results
+(controller
+ gcd-loop
+  (assign a (op read))
+  (assign b (op read))
+ test-b
+  (test (op =) (reg b) (const 0))
+  (branch (label gcd-done))
+  (assign t (op rem) (reg a) (reg b))
+  (assign a (reg b))
+  (assign b (reg t))
+  (goto (label test-b))
+ gcd-done
+  (perform (op print) (reg a))
+  (goto (label gcd-loop)))
+
+;; Action is different from operation: 
+;; - does not produce an output value to be stored in a register
+;; - read is an operation, print is an action
